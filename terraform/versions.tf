0cecb20801c3c591495a07c1e9e57cb4642eb7de
Aleksandr Khokhlov <hohlovap@gmail.com>
2022-01-16 15:38:36 +0300

    fix yc version

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = "~> 0.35"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 0.12"
 }
